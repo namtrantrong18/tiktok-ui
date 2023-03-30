3cbb057ea8ed0fb9b802c1872496c918ac51c815
namtrantrong18 <namtrantrong18@gmail.com>
2023-03-30 13:54:49 +0700

    Xây dựng UI phần Header #3

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,27 @@
+import { faCircleCheck } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames/bind';
+import styles from './AccountItem.modal.scss';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img
+                className={cx('avatar')}
+                src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/9ce896adfa0deb8f2eba8e7e5861a1e3~c5_100x100.jpeg?x-expires=1680267600&x-signature=HpFcA47NBgqAwYthebvv1ed217o%3D"
+                alt="Hoa"
+            />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>Nguyen Van A</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCircleCheck} />
+                </h4>
+                <span className={cx('username')}>nguyenvana</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
